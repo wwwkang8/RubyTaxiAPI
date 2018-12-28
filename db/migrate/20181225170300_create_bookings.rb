4d105e15282c3f5bc28603e0dfc4d8f767f238ea
JeongHo <wwwkang8@gmail.com>
2018-12-28 13:36:36 +0900

    Created migration : taxi => nullable, updated_at => nullable

--- db/migrate/20181225170300_create_bookings.rb
+++ db/migrate/20181225170300_create_bookings.rb
@@ -3,7 +3,7 @@ class CreateBookings < ActiveRecord::Migration[5.2]
     create_table :bookings do |t|
       t.string :destination
       t.string :status
-      t.integer :taxi
+      t.integer :taxi, :null => true
       t.datetime :created_at
       t.datetime :updated_at
       t.references :user, foreign_key: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_25_170300) do
+ActiveRecord::Schema.define(version: 2018_12_28_043058) do
 
   create_table "bookings", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "destination"
     t.string "status"
     t.integer "taxi"
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "updated_at"
     t.bigint "user_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
