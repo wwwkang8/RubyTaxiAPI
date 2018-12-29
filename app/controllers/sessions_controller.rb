d7c2d5b851652df62b9506eec846f97cd4dfb391
JeongHo <wwwkang8@gmail.com>
2018-12-29 16:40:29 +0900

    Add create_token, check_valid_password method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,8 @@ class SessionsController < ApplicationController
       session[:user_id]=@user.id
       @msg = {
           message: "You have successfully logged in",
-          user: User.find_by(id: session[:user_id])
+          user: User.find_by(id: session[:user_id]),
+          session_id: session[:user_id]
       }
       render json: @msg
     else
@@ -30,6 +31,28 @@ class SessionsController < ApplicationController
         user: User.find_by_id(session[:user_id])
     }
     render json: @msg
+  end
+
+  def create_token
+    user = User.where(email: params[:email]).first
+
+    if user.present?&&check_valid_password(user, params[:pwd])
+      render json: user.as_json(only: [:id, :email]), status: :created
+    else
+      error_message_response('No user found. Please check email/password')
+    end
+
+  end
+
+  def check_valid_password(user, password)
+    user.pwd == password
+  end
+
+  def error_message_response(message, errors = nil)
+    render(json:{
+        message: message,
+        errors: errors
+    })
   end
 
 end
