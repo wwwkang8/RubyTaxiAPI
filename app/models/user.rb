330de174c5025111c042c00b649598254799c89e
JeongHo <wwwkang8@gmail.com>
2019-01-01 23:47:13 +0900

    Change User.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,24 +2,6 @@ class User < ApplicationRecord
 
   acts_as_token_authenticatable
 
-=begin
-  devise :invitable, :database_authenticatable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :lockable
-=end
-  # attr_accessor :email, :pwd, :usertype
-
-=begin
-  devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable
-=end
-
-=begin
-    def initialize(email, pwd, usertype)
-      @email=email, @pwd=pwd, @usertype=usertype
-    end
-=end
-
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: { case_sensitive: false},
             format: {with: VALID_EMAIL_REGEX}, length: {minimum:3, maximum:50}
@@ -27,14 +9,5 @@ class User < ApplicationRecord
   validates :pwd, presence: true, length: {minimum:3, maximum:25}
 
   has_many :bookings
-
-  #has_secure_password
-=begin
-  alias_method :authenticate, :valid_password?
-
-  def self.from_token_payload(payload)
-    self.find payload["sub"]
-  end
-=end
 
 end
