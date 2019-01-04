6e9d080652254f45b96ec85d68d2ca4c5de7aff9
강정호 <jeongho.kang@dramancompany.com>
2019-01-04 09:40:06 +0900

    회원가입시 비밀번호 관련 에러메시지 추가

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,13 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     # 잘못 입력된 usertype에 대해서 에러메시지 리턴
-    return error_message_response('Please choose either passenger or driver in usertype') if @user.usertype != 'passenger' && @user.type != 'driver'
+    return error_message_response('Please choose either passenger or driver in usertype') if @user.usertype != 'passenger' && @user.usertype != 'driver'
+
+    # 잘못 입력된 비밀번호에 대해서 에러메시지 리턴
+    return error_message_response('Password is empty') if @user.pwd.nil?
+
+    # 6자리 미만으로 입력된 비밀번호에 대해서 에러메시지 리턴
+    return error_message_response('Please write password at least 6 characters') if @user.pwd.length < 6
 
     if @user.save
       @msg = {
@@ -44,6 +50,7 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
+    # 여기서 어떻게 하면 성공혹은 실패 메시지를 보낼 수 있을까??
     @user.destroy
   end
 
